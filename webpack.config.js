ac52705685a3e6f4722003d94d9d8effe20de4d9
zhangyurui <zhangyurui@beisen.com>
2020-08-26 16:53:35 +0800

    feat: add hot webpack and start scripts

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,35 @@
 const webpack = require('webpack');
 const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = function(options) {
   return {
     ...options,
     entry: ['webpack/hot/poll?100', './src/main.ts'],
     watch: true,
+    target: 'node',
     externals: [
       nodeExternals({
-        whitelist: ['webpack/hot/poll?100'],
+        allowlist: ['webpack/hot/poll?100'],
       }),
     ],
-    plugins: [...options.plugins, new webpack.HotModuleReplacementPlugin()],
+    module: {
+      rules: [
+        {
+          test: /.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/,
+        },
+      ],
+    },
+    mode: 'development',
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins: [new webpack.HotModuleReplacementPlugin()],
+    output: {
+      path: path.join(__dirname, 'dist'),
+      filename: 'server.js',
+    },
   };
 }
